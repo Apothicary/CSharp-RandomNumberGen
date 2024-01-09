85e42ca0f1964b72edaa5f8108c36c089269bc3a
Nathan Strang <n.strang95@gmail.com>
2024-01-09 17:15:00 +0000

    Adding program code

---
+++ RandomNumberGen/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RandomNumberGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();          // Instantiates a random object called "random"
+            int num = random.Next(1,7);            // Generates a random number within a set range (1-6 in this case)
+            double number = random.NextDouble();   // Generates a random number between 0 - 1
+            
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
